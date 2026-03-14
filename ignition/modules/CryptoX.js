3b3dda263746b8f50459d50adaaa67f354248c29
Parth Verma <parthverma368@gmail.com>
2026-03-14 23:04:05 +0530

    remove hardhat files and private key from repo

--- ignition/modules/CryptoX.js
+++
@@ -1,6 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("CryptoX", (m) => {
-  const cryptoX = m.contract("CryptoX");
-  return { cryptoX };
-});
